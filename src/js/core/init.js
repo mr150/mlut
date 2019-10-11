8568cb8bc43328a487381cd46aba19dc9576babf
mr150 <im150@bk.ru>
2019-10-11 09:06:25 +0300

    Fixed regexes in init.js. Added info about `js` and `nojs` states

--- src/js/core/init.js
+++ src/js/core/init.js
@@ -1,7 +1,10 @@
 /*
 Init
 
-Implements `js` and `nojs` [states](section-concepts.html#kssref-concepts-utility-states).
+Implements `js` and `nojs` [states](section-concepts.html#kssref-concepts-utility-states) for utilities.
+
+When loading `init.js`, it will remove `js` state from the utilities names and they will work as usual.
+Utilities with `nojs` state also will be removed from the markup when loading this script.
 
 Weight: 0
 
@@ -13,12 +16,13 @@ Styleguide: core-scripts.init
 ;(function(){
 	"use strict"
 
-	var nodesWithJsUtils = document.querySelectorAll("[class*='js_']"),
+	var nodesWithJsUtils = document.querySelectorAll("[class*='js']"),
 			i = 0;
 
 	for(i = 0; i < nodesWithJsUtils.length; i++) {
 		nodesWithJsUtils[i].className = nodesWithJsUtils[i].className
-			.replace(/(?:\ )?nojs_(?:[^\ \n]*)/g, "")
-			.replace(/js_/g, "");
+			.replace(/(\ |^)nojs[_\-](?:[^\ \n]*)/g, "")
+			.replace(/(\ |^)js[_\-]/g, " ")
+			.trim();
 	}
 }());
