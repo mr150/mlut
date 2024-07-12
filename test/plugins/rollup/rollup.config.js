3938d47f97fc35083b931da4e08b0e544be28d09
mr150 <im150@bk.ru>
2024-07-12 14:04:48 +0300

    test(plugins): improve the Rollup plugin test

--- test/plugins/rollup/rollup.config.js
+++ test/plugins/rollup/rollup.config.js
@@ -1,22 +1,40 @@
 import { join } from 'node:path';
 import { rollup } from '@mlut/plugins';
+import serve from 'rollup-plugin-serve'
+import livereload from 'rollup-plugin-livereload'
+import htmlTemplate from 'rollup-plugin-generate-html-template';
 
 const __dirname = new URL('.', import.meta.url).pathname;
 
 const mlut = rollup({
-	input: join(__dirname, 'style.scss'),
-	output: join(__dirname, 'dist/style.css'),
-	minify: true,
-	autoprefixer: true,
+	// other options in the input file
+	input: join(__dirname, 'src/style.scss'),
 });
 
+const plugins = [
+	mlut,
+	htmlTemplate({
+		template: join(__dirname, 'src/index.html'),
+		target: join(__dirname, 'dist/index.html'),
+		attrs: ['type="module"'],
+	}),
+];
+
+if (process.env.ROLLUP_WATCH) {
+	plugins.push(
+		serve(join(__dirname, 'dist')),
+		livereload({
+			// prevents race condition with CSS file writing by mlut and livereload
+			delay: 100,
+		}),
+	);
+}
+
 export default {
-	input: join(__dirname, 'index.js'),
-	plugins: [
-		mlut,
-	],
+	input: join(__dirname, 'src/index.js'),
+	plugins,
 	output: {
-		file: join(__dirname, 'dist/main.js'),
+		dir: join(__dirname, 'dist'),
 		format: 'es',
 	},
 };
--- test/plugins/rollup/src/index.js
+++ test/plugins/rollup/src/index.js
@@ -2,7 +2,7 @@ import { widget } from './widget.js';
 
 const app = `
 	<div class="wrapper Mm7">
-		<h1 class="Fns5r C-red">Rollup plugin test</h1>
+		<h1 class="Fns5r C-purple">Rollup plugin test!</h1>
 		<p>Lorem about mlut</p>
 	</div>
 `;
